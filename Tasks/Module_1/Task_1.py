f69cfbe7d8bb8a5efe4110b942663a1849ec1854
Vany2002 <ivanrazin2002@gmail.com>
2022-09-13 11:28:07 +0300

    New

--- Tasks/Module_1/Task_1.py
+++ Tasks/Module_1/Task_1.py
@@ -3,5 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-print(7777)
+a = int(input())
+b = int(input())
+c = int(input())
+print(a+b+c)
+
 
