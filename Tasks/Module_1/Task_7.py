02887e1d56c9d0bfee2c9da0fbe53c23400de958
Vany2002 <ivanrazin2002@gmail.com>
2022-09-14 07:49:35 +0300

    New

---
+++ Tasks/Module_1/Task_7.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+s = a * 3600
+S = b * 60
+a2 = int(input())
+b2 = int(input())
+g = int(input())
+f = a2 * 3600
+F = b2 * 60
+print(f + F + g - s - S - c)
