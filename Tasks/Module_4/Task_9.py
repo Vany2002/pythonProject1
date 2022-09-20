f6cee78cb501c2b846c3fd7105de642e71d579bf
Vany2002 <ivanrazin2002@gmail.com>
2022-09-21 00:58:07 +0300

    New

--- Tasks/Module_4/Task_9.py
+++ Tasks/Module_4/Task_9.py
@@ -3,7 +3,7 @@ b = int(input())
 c = int(input())
 if a == b and a == c:
     print(3)
-elif a == b and a != c or a == c and a != b or b == a and b != c or b == c and b != a:
-    print(2)
-else:
+elif a != b and a != c:
     print(0)
+else:
+    print(2)
