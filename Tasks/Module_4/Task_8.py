a2c65c2491379f589cf11c2c98ffb08fce1b68ad
Vany2002 <ivanrazin2002@gmail.com>
2022-09-21 00:42:47 +0300

    New

---
+++ Tasks/Module_4/Task_8.py
@@ -0,0 +1,7 @@
+a = int(input())
+if a == 1 or a == 3 or a == 5 or a == 7 or a == 8 or a == 10 or a == 12:
+    print(31)
+elif a == 2:
+    print(28)
+elif a == 4 or a == 6 or a == 9 or a == 11:
+    print(30)
