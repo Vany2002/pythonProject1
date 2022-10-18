962cfe0ec175f26c79c1b684a68432d9175a6555
Vany2002 <ivanrazin2002@gmail.com>
2022-10-18 11:42:38 +0300

    New

---
+++ Tasks/Dop zadaniya/4.py
@@ -0,0 +1,5 @@
+def gms_to_deg(degrees, minutes, seconds):
+
+def deg_to_rad(deg):
+
+def rad_to_deg(rad):
--- Tasks/Dop zadaniya/5.py
+++ Tasks/Dop zadaniya/5.py
@@ -1,6 +1,11 @@
 def deg_to_gms(deg, formats='string'):
-
-def gms_to_deg(degrees, minutes, seconds):
-
-def deg_to_rad(deg):
-def deg_to_rad(rad):
+    d = int(deg)
+    m = int((deg - d) * 60)
+    s = (deg - d - m / 60) * 3600.00
+    s = round(s, 2)
+    if formats == 'string':
+        answer = f'{d}°, {m}, {s}'
+    elif formats == 'num':
+        answer = (d, m, s)
+    else:
+        answer = None
