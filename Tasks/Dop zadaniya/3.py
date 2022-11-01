ff6d20465e653be84026e08e8a60262f3c90958b
Vany2002 <ivanrazin2002@gmail.com>
2022-11-01 11:03:32 +0300

    New

--- Tasks/Dop zadaniya/3.py
+++ Tasks/Dop zadaniya/3.py
@@ -1,65 +1,62 @@
-contacts = [
-    {
-        "name": "Ivan",
-        "phone": "+79031035012"
-    }
-]
+def get_num():
+    num = input("Введите номер телефона: ")
+    num = num.replace(" ", "").replace("-", "")
 
-def list(contacts):
-    print(''.format('Name', 'Phone'))
-    for contact in contacts:
-        print('Имя: {}, Номер: {}'.format(
-            contact['name'],
-            contact['phone']
-        ))
+    if (num[0] == "8") and (len(num) == 11):
+        num = num.replace("8", "+7", 1)
+    if (num[0] == "7") and (len(num) == 11):
+        num = "+" + num
+    if (num[0] == "9") and (len(num) == 10):
+        num = "+7" + num
+    if (len(num) == 12) and (num[1:].isdigit()):
+        return(num)
+    else:
+        print("Неправильно набран номер!")
+        return get_num()
 
-def delete(contacts):
-    print("Введите имя контакта: ")
-    name = input('> ')
-    for contact in contacts:
-        if contact['name'] == name:
-            contacts.remove(contact)
-            print("Вы удалили контакт %s " % name)
+def get_name():
+    name = input("Введите имя: ")
+    name = name.title()
+    return name
 
-def edit(contacts):
-    print("Введите имя контакта: ")
-    name = input('> ')
-    for contact in contacts:
-        if contact['name'] == name:
-            contacts.remove(contact)
-            print("Вы удалили контакт %s " % name)
+def get_contact(dict, names, num):
+    dict[names] = num
+    print("Контакт добавлен")
+    return dict
 
-def add(contacts):
-    print("Введите имя контакта:")
-    name = input("> ")
-    print("Введите телефон контакта:")
-    phone = input("> ")
-    new_contact = {
-        'name': name,
-        'phone': phone
-    }
-    contacts.append(new_contact)
-    print('Контакт сохранён')
+def remove_contact (dict, names):
+    if not (names in dict):
+        print("Нет такого номера!")
+        return False
+    del dict[names]
+    return dict
 
-print("""Введите команду:
-* list - чтобы посмотреть список контактов.
-* add  - добавить контакт
-* del  - удаление контакта
-* edit - изменение контакта 
-* exit - выход""")
+def show_dict(dict):
+    print("Список контактов")
+    for i in dict:
+        print(i, dict[i])
+
+def change_num(dict, names):
+    if not (names in dict):
+        print("Нет такого номера!")
+        return False
+    dict[names] = get_num()
+    print("Контакт изменён")
+    return(dict)
+
+i = 0
+dict = {}
 
 while True:
-    print("\nВведите команду: ")
-    command = input('> ')
-    if command == 'list':
-        list(contacts)
-    elif command == 'add':
-        add(contacts)
-    elif command == 'del':
-        delete(contacts)
-    elif command == 'edit':
-        edit(contacts)
-    elif command == 'exit':
+    i = int(input("Добро пожаловать! Введите команды от 1 до 5:\n1 - Добавить контакт\n2 - Удалить контакт\n3 - Просмотреть телефонную книгу\n4 - Изменить номер телефона\n5 - Завершить работу"))
+    if i == 1:
+        get_contact(dict, get_name(), get_num())
+    if i == 2:
+        remove_contact(dict, get_name())
+    if i == 3:
+        show_dict(dict)
+    if i == 4:
+        change_num(dict, get_name())
+    if i == 5:
+        print("Завершение работы...")
         break
-    else:
-        print("Неизвестная команда")
