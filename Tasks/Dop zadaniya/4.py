3c828dfe9ce882c0cb92ab8d7c704838a657dd3d
Vany2002 <ivanrazin2002@gmail.com>
2022-11-01 11:09:25 +0300

    New

--- Tasks/Dop zadaniya/4.py
+++ Tasks/Dop zadaniya/4.py
@@ -1,5 +1,35 @@
-def gms_to_deg(degrees, minutes, seconds):
+from math import pi
 
+# Получает десятичные градусы - возвращает градусы, минуты, секунды
+def deg_to_gms(deg, formats='string'):
+    d = int(deg)
+    m = int(deg - d) * 60
+    s = ((((deg - d) * 60) - m) * 60)
+    s = round(s, 5)
+
+    if formats == 'string':
+        answer = f'{d}° {m}m {s}s'
+    elif formats == 'num':
+        answer = (d,m,s)
+    else:
+        answer = None
+    return answer
+
+
+
+# Получает градусы, минуты, секунды - возвращает десятичные градусы
+def gms_to_deg(d,m,s):
+    return d + (m / 60) + (s/3600)
+
+# Получает десятичные градусы - возвращает радианы
 def deg_to_rad(deg):
+    return deg * (pi / 180)
 
+# Получает радианы - возвращает десятичные градусы
 def rad_to_deg(rad):
+    return rad * (180 / pi)
+
+print(deg_to_gms(36.97, formats="num"))
+print(gms_to_deg(36, 58, 12))
+print(deg_to_rad(36.97))
+print(rad_to_deg(deg_to_rad(36.97)))
