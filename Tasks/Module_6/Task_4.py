02a7f8a5a814acb647f538f2270323794fc143ed
Vany2002 <ivanrazin2002@gmail.com>
2022-12-09 01:07:41 +0300

    New

--- Tasks/Module_6/Task_4.py
+++ Tasks/Module_6/Task_4.py
@@ -1,3 +1,12 @@
 a = [int(s) for s in input().split()]
-for i in range(len(a) - 1):
-    print(max(a), min(a))
+max = -10000000
+min = 10000000
+for i in range(0, len(a)):
+    if a[i] > max:
+        max = a[i]
+    elif a[i] < min:
+        min = a[i]
+index_of_max_num = a.index(max)
+index_of_min_num = a.index(min)
+a[index_of_min_num], a[index_of_max_num] = a[index_of_max_num], a[index_of_min_num]
+print(" ".join(map(str, a)))
