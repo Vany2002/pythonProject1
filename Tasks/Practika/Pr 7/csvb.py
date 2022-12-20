7f623df177b50b73fc4d070e8e5158b963cab84b
Vany2002 <ivanrazin2002@gmail.com>
2022-12-20 11:16:13 +0300

    New

--- Tasks/Practika/Pr 7/csvb.py
+++ Tasks/Practika/Pr 7/csvb.py
@@ -1,11 +1,34 @@
 import csv
 
+
 def get_books(filename: str, name: str):
-    with open(filename, 'r') as file:
-        read = csv.reader(file, delimiter='|')
-        res = list
-        for row in read:
-            if row.__contains__(name):
-                res.append(row)
-        return res
-books = get_books("books.csv", "python")
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter='|')
+
+        result = [[]]
+        result.clear()
+
+        for row in reader:
+            if ''.join(row).__contains__(name):
+                result.append(row)
+
+        return result
+
+def get_totals(data: [[]], sum = 0, add = 0):
+    result = [[]]
+    result.clear()
+
+    for row in data:
+        count = int(row[3]) * float(row[4])
+        if count < sum:
+            sum += add
+        result.append([row[0], count])
+
+    return result
+
+
+books = get_books("books.csv", "Python")
+result = get_totals(books)
+
+for i in result:
+    print(i)
--- Tasks/Practika/Pr 8/main.py
+++ Tasks/Practika/Pr 8/main.py
@@ -1,31 +1,72 @@
+import re
+import pymorphy2
 from translate import Translator
+import translate
 
-if __name__ == '__main__':
-    with open("dialog.txt", "r") as file:
-        text = file.read()
-        text = text.lower()
-        file.close()
-    text_lst = text.split()
-    dict = {}
-    for i in text_lst:
+def read_file(filename: str):
+    f = open(filename, 'r', encoding='utf-8')
+    words = f.read()
+    words = re.sub(r'[^\w\s]', '', words)
+    words = re.sub(r"\d+", '', words)
+    return words.split()
+
+def list_words(words: [str]):
+    wordsCount = {}
+
+    for word in words:
+        if len(word) > 1:
+            if not word in wordsCount:
+                wordsCount[word] = 0
+            wordsCount[word] += 1
+
+    sorted_words = {}
+    sorted_keys = sorted(wordsCount, key=wordsCount.get)
+    sorted_keys.reverse()
+
+    for values in sorted_keys:
+        sorted_words[values] = wordsCount[values]
+
+    return sorted_words
+
+def translate(words: {}):
+    morph = pymorphy2.MorphAnalyzer()
+    translator = Translator(from_lang="ru", to_lang="en")
+
+    translate_words = {}
+    for key, value in words.items():
+        en_word = str
         try:
-            dict[i] = dict[i] + 1
+            normal_word = morph.parse(key)[0].normal_form
+            en_word = translator.translate(normal_word)
+            if en_word == None:
+                raise Exception
         except:
-            dict[i] = 0
-    sorted_values = sorted(dict.values(), reverse=True)
-    sorted_dict = {}
-    for i in sorted_values:
-        for k in dict.keys():
-            if dict[k] == i:
-                sorted_dict[k] = dict[k]
-                dict[k] = -1
-    translated_lst = []
-    for k, v in sorted_dict.items():
-        trans = Translator(from_lang='ru', to_lang='en')
-        word = trans.translate(k)
-        translated_lst.append([k, word.lower(), v])
-    with open("translated.txt", "w+") as file:
-        for i in translated_lst:
-            file.write(f"{i[0]} | {i[1]} | {i[2]} \n")
-        file.close()
-    print(translated_lst)
+            en_word = 'untranslated'
+
+        print(en_word)
+        translate_words[en_word] = value
+
+    return translate_words
+
+
+def write_file(filename, translate_words: {}, sorted_words: {}):
+    ru_words = []
+    en_words = []
+    words_count = []
+
+    for key, value in sorted_words.items():
+        ru_words.append(key)
+
+    for key, value in translate_words.items():
+        en_words.append(key)
+        words_count.append(value)
+
+    f = open(filename, 'a')
+    for i in range(len(translate_words)):
+        line = "{0} - {1} {2} \n".format(en_words[i], ru_words[i], words_count[i])
+        f.write(line)
+
+words = read_file('dialog.txt')
+sorted_words = list_words(words)
+transleted_words = translate(sorted_words)
+write_file('TranslatedWords.txt', transleted_words, sorted_words)
