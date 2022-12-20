08af4af7e88319d0f9122c6f76aef6e9c5f541af
Vany2002 <ivanrazin2002@gmail.com>
2022-12-20 13:55:21 +0300

    New

--- Tasks/Bilet/Hesh.py
+++ Tasks/Bilet/Hesh.py
@@ -1,16 +1,17 @@
-def get_strin():
-    strin = input("Введите строку: ")
-    strin = strin.replace(" ", "").replace("-", "")
-    strin = strin.title()
-    if (strin[0] != "#") and (len(strin) < 140):
-        strin = "#" + strin
-    else:
-        print("Неправильно введена строка!")
-        return get_strin()
-i = 0
-while True:
-    i = int(input(""))
-    if i == 1:
-        get_strin()
-        print("Завершение работы...")
-        break
+import re
+
+def get_hash_tag(input_string):
+    if len(input_string) > 140 or len(input_string) == 0:
+        return False
+
+    input_string = re.sub(r'[^\w\s]', '', input_string)
+
+    result = input_string.title()
+    result = result.replace(' ', '')
+    return f'#{result}'
+
+print(get_hash_tag('weviruv wevnon; rwn;erow rweoi;'))
+
+
+
+
