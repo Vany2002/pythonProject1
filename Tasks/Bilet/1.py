b660b10a2efe3e43fe3eed4bb4300b26ba70a56e
Vany2002 <ivanrazin2002@gmail.com>
2022-12-21 17:41:18 +0300

    New

--- Tasks/Bilet/1.py
+++ Tasks/Bilet/1.py
@@ -14,27 +14,27 @@ current_time = now.strftime("%H:%M:%S")
 info.append(f'Time: {current_time}\n')
 
 temp = re.findall(r'(?:temp":)([^,]+)', resp)
-info.append(f'Temperature: {temp}\n')
+info.append(f'Temperature: {temp[0]}\n')
 
 humidity = re.findall(r'(?:humidity":)([^,]+)', resp)
-info.append(f'Air humidity: {humidity}\n')
+info.append(f'Air humidity: {humidity[0]}\n')
 
 speed = re.findall(r'(?:speed":)([^,]+)', resp)
-info.append(f'Wind speed: {speed}\n')
+info.append(f'Wind speed: {speed[0]}\n')
 
 pressure = re.findall(r'(?:pressure":)([^,]+)', resp)
-info.append(f'Atmosphere pressure: {pressure}\n')
+info.append(f'Atmosphere pressure: {pressure[0]}\n')
 
-name = re.findall(r'(?:name":)([^,]+)', resp)
-info.append(f'city: {name}\n')
+name = re.findall(r'(?:name":")([^,]+)(?:")', resp)
+info.append(f'City: {name[0]}\n')
 
 for i in info:
     print(i)
 
 f = open('text.txt', 'r+', encoding='utf-8')
-f.write(f'Time: {now}\n')
-f.write(f'City: {name}\n')
-f.write(f'Temperature: {temp}\n')
-f.write(f'Air humidity: {humidity}\n')
-f.write(f'Wind speed: {speed}\n')
-f.write(f'Atmosphere pressure: {pressure}\n')
+f.write(f'Time: {current_time}\n')
+f.write(f'City: {name[0]}\n')
+f.write(f'Temperature: {temp[0]}\n')
+f.write(f'Air humidity: {humidity[0]}\n')
+f.write(f'Wind speed: {speed[0]}\n')
+f.write(f'Atmosphere pressure: {pressure[0]}\n')
