b8e346504c5ca2218287052bf4995c56bdc836d9
Vany2002 <ivanrazin2002@gmail.com>
2022-12-21 12:02:31 +0300

    New

--- Tasks/Bilet/Pig_it.py
+++ Tasks/Bilet/Pig_it.py
@@ -1,11 +1,21 @@
+import re
+
 def pig_it(input_line):
     input_words = input_line.split(' ')
 
     result_words = []
     for word in input_words:
-        result_words.append(f'{word[1:]}{word[0]}ay')
+        clear_word = re.sub(r'[^\w\s]', '', word)
+        special_symbols = re.sub(r'[A-Za-z]', '', word)
+
+        if len(clear_word) > 1:
+            result_words.append(f'{clear_word[1:]}{clear_word[0]}ay{special_symbols}')
+        elif len(clear_word) == 1:
+            result_words.append(f'{clear_word}ay{special_symbols}')
+        else:
+            result_words.append(special_symbols)
 
     return ' '.join(result_words)
 
 
-print(pig_it('Pig latin is cool'))
+print(pig_it('Pig latin, is cool !'))
