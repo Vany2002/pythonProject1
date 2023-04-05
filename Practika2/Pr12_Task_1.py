b3873b59d465256c969c77848772f4d15960d41a
Vany2002 <ivanrazin2002@gmail.com>
2023-04-05 15:09:05 +0300

    New

---
+++ Practika2/Pr12_Task_1.py
@@ -0,0 +1,51 @@
+# coding=<encoding name>
+class Student:
+    def __init__(self, name="Ivan", age=18, groupNumber="10A"):
+        self.name = name
+        self.age = age
+        self.groupNumber = groupNumber
+
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+
+    def getGroupNumber(self):
+        return self.groupNumber
+
+    def setNameAge(self, name, age):
+        self.name = name
+        self.age = age
+
+    def setGroupNumber(self, groupNumber):
+        self.groupNumber = groupNumber
+
+    def printInfo(self):
+        print("Name: ", self.name)
+        print("Age: ", self.age)
+        print("GroupNumber: ", self.groupNumber)
+
+Alex = Student("Alex", 20, "11B")
+Alex.printInfo()
+print("")
+
+Peter = Student("Peter", 17, "10B")
+Peter.printInfo()
+print("")
+
+Vitya = Student()
+Vitya.setNameAge("Vitya", 19)
+Vitya.setGroupNumber("11A")
+Vitya.printInfo()
+print("")
+
+Gena = Student("Gena", 17)
+Gena.setGroupNumber("10A")
+Gena.printInfo()
+print("")
+
+Dima = Student()
+Dima.setNameAge("Dima", 18)
+Dima.setGroupNumber("11A")
+Dima.printInfo()
---
+++ Practika2/Pr12_Task_2.py
@@ -0,0 +1,22 @@
+class Math:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def addition(self):
+        return self.a + self.b
+
+    def multiplication(self):
+        return self.a * self.b
+
+    def division(self):
+        return self.a / self.b if not self.b == 0 else 0
+
+    def subtraction(self):
+        return self.a - self.b
+
+math = Math(235, 23)
+print(math.addition())
+print(math.multiplication())
+print(math.division())
+print(math.subtraction())
